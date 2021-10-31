fd8ac08ce397e9668ec566e71c3913401f6960c9
Andy Jung <hello@andyjung.dev>
2021-10-31 13:38:33 -0400

    Initial commit

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+const { ModalContextProvider } = require("@chakra-ui/modal");
+const { m } = require("framer-motion");
+
+const options = {
+    singleQuote: true,
+    endOfLine: 'lf',
+    arrowParens: 'avoid',
+    tabWidth: 4,
+    semi: false
+}
+module.exports = options
