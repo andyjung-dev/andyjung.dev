0f1aeff5596fe4ad2b3dfd32836b98b0717e69ee
Andy Jung <hello@andyjung.dev>
2021-10-31 20:29:04 -0400

    update stuff

--- pages/stuff.js
+++ pages/stuff.js
@@ -2,20 +2,18 @@
 
 
 import { Container, Heading, SimpleGrid, Divider } from '@chakra-ui/react'
-import Layout from '../components/layouts/article'
-import Section from '../components/section'
-import { WorkGridItem } from '../components/grid-item'
+import Layout from '@/components/layouts/article'
+import Section from '@/components/section'
+import { WorkGridItem } from '@/components/grid-item'
 
-import thumbInkdrop from '../public/images/works/inkdrop_eyecatch.png'
-import thumbWalknote from '../public/images/works/walknote_eyecatch.png'
-import thumbFourPainters from '../public/images/works/the-four-painters_eyecatch.jpg'
-import thumbMenkiki from '../public/images/works/menkiki_eyecatch.png'
-import thumbModeTokyo from '../public/images/works/modetokyo_eyecatch.png'
-import thumbStyly from '../public/images/works/styly_eyecatch.png'
-import thumbPichu2 from '../public/images/works/pichu2_eyecatch.png'
-import thumbFreeDBTagger from '../public/images/works/freedbtagger_eyecatch.png'
-import thumbAmembo from '../public/images/works/amembo_eyecatch.png'
+const items = [
+{id: 354, thumbnail:"https://picsum.photos/200/300", title: "title1", content: "hi there"},
+{id: 454, thumbnail:"https://picsum.photos/200/300", title: "title2", content: "hi there"},
+{id: 554, thumbnail:"https://picsum.photos/200/300", title: "title3", content: "hi there"},
+{id: 654, thumbnail:"https://picsum.photos/200/300", title: "title4", content: "hi there"}
 
+
+]
 const Works = () => (
   <Layout title="Works">
     <Container>
@@ -42,14 +40,13 @@ const Works = () => (
       </Section>
 
       <SimpleGrid columns={[1, 1, 2]} gap={6}>
-        {rows.map(i => {
-            return 
-            <Section delay={0.5}>
+        {items.map(i => 
+            <Section delay={0.5} key={i}>
             <WorkGridItem id={i.id} thumbnail={i.thumbnail} title={i.title}>
               { i.content }
             </WorkGridItem>
-            </Section>;
-        })}
+            </Section>
+        )}
 
       </SimpleGrid>
 
